08007223338697b90e2228f64d5fcda20955dfcd
comfyanonymous <121283862+comfyanonymous@users.noreply.github.com>
2023-05-17 01:34:43 -0400

    Create sampler_rescalecfg.py

---
+++ sampler_rescalecfg.py
@@ -0,0 +1,34 @@
+import torch
+
+
+class RescaleClassifierFreeGuidance:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "model": ("MODEL",),
+                              "multiplier": ("FLOAT", {"default": 0.7, "min": 0.0, "max": 1.0, "step": 0.01}),
+                              }}
+    RETURN_TYPES = ("MODEL",)
+    FUNCTION = "patch"
+
+    CATEGORY = "custom_node_experiments"
+
+    def patch(self, model, multiplier):
+        
+        def sampler_tonemap_reinhard(cond, uncond, cond_scale):
+            x_cfg = uncond + cond_scale * (cond - uncond)
+            ro_pos = torch.std(cond, dim=(1,2,3), keepdim=True)
+            ro_cfg = torch.std(x_cfg, dim=(1,2,3), keepdim=True)
+
+            x_rescaled = x_cfg * (ro_pos / ro_cfg)
+            x_final = multiplier * x_rescaled + (1.0 - multiplier) * x_cfg
+
+            return x_final
+
+        m = model.clone()
+        m.set_model_sampler_cfg_function(sampler_tonemap_reinhard)
+        return (m, )
+
+
+NODE_CLASS_MAPPINGS = {
+    "RescaleClassifierFreeGuidanceTest": RescaleClassifierFreeGuidance,
+}
