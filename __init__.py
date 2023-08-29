fbadedc06c3a2a2637addf5b7f55c4f8c5e8b9fb
comfyanonymous <121283862+comfyanonymous@users.noreply.github.com>
2023-08-29 18:14:47 -0400

    Fix __init__.py

--- __init__.py
+++ __init__.py
@@ -16,6 +16,7 @@ for module_name in node_list:
     imported_module = importlib.import_module(".{}".format(module_name), __name__)
 
     NODE_CLASS_MAPPINGS = {**NODE_CLASS_MAPPINGS, **imported_module.NODE_CLASS_MAPPINGS}
-    NODE_DISPLAY_NAME_MAPPINGS = {**NODE_DISPLAY_NAME_MAPPINGS, **imported_module.NODE_DISPLAY_NAME_MAPPINGS}
+    if hasattr(imported_module, "NODE_DISPLAY_NAME_MAPPINGS"):
+        NODE_DISPLAY_NAME_MAPPINGS = {**NODE_DISPLAY_NAME_MAPPINGS, **imported_module.NODE_DISPLAY_NAME_MAPPINGS}
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
